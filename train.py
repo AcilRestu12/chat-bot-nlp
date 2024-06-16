39369906f2f8257643d4d1673fb40d18bd25479c
AcilRestu12 <arieskarestu214@gmail.com>
2024-06-17 01:42:39 +0700

    Part 3 - Create model using PyTorch and training model

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import json
 import numpy as np
-from nltk_utils import tokenize, stem, bag_of_words
 
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from nltk_utils import tokenize, stem, bag_of_words
+from model import NeuralNet
 
 # Open file json
 with open('intents.json', 'r') as f:
@@ -34,7 +35,7 @@ all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))      
 tags = sorted(set(tags)) 
                
-print(tags)
+# print(tags)
 
 # Define train data
 x_train = []
@@ -59,8 +60,8 @@ class ChatDataset(Dataset):
         self.n_samples = len(x_train)
         self.x_data = x_train
         self.y_data = y_train
-        
-    def __getItem__(self, index):
+    
+    def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
     
     def __len__(self):
@@ -68,12 +69,46 @@ class ChatDataset(Dataset):
     
 # Define Hyperparameter
 batch_size = 8
-    
+hidden_size = 8
+input_size = len(x_train[0])
+output_size = len(tags)
+learning_rate = 0.001
+num_epochs = 1000
+# print(input_size, len(all_words))
+# print(output_size, tags)
+
 # Create dataset
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+# train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+# train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers = 2, persistent_workers=True)
+
+# Checking cuda
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Create model
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# Loss & optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     
-    
-    
-    
-    
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device, dtype=torch.int64)
+        
+        # Forward
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch + 1) % 100 == 0:
+        print(f'epoch : {epoch+1}/{num_epochs}, loss={loss.item():.4f}')
+        
+print(f'final loss, loss={loss.item():.4f}')
+
